91249234aa1751f8ed140e81b6e502a14b17a1dc
Ross Delinger <rdelinger@helixoide.com>
2012-08-14 16:49:42 -0400

    Basic model, with users, and topics

--- fuss/models.py
+++ fuss/models.py
@@ -1,7 +1,9 @@
 from sqlalchemy import (
     Column,
     Integer,
-    Text,
+    ForeignKey,
+    Unicode,
+    Table,
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -9,11 +11,23 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import (
     scoped_session,
     sessionmaker,
+    relationship,
     )
 
 from zope.sqlalchemy import ZopeTransactionExtension
 
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
+Base.query = DBSession.query_property()
 
 
+class FASUser(Base):
+    __table__ = 'users'
+    id = Column(Integer, unique=True, primary_key=True)
+    username = Column(Unicode(128), unique=True)
+    topics = relationship("Topic", backref='fasuser')
+
+
+class Topic(Base):
+    __table__ = 'topics'
+    name = Column(Unicode(128), unique=True)
