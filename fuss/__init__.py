2342bac74ba4805e2b1ac29b4f9d2d768d4e7167
Ross Delinger <rdelinger@helixoide.com>
2012-08-15 12:17:47 -0400

    added the index route to __init__.py

--- fuss/__init__.py
+++ fuss/__init__.py
@@ -10,6 +10,7 @@ def main(global_config, **settings):
     DBSession.configure(bind=engine)
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('index', '/')
     config.scan()
     return config.make_wsgi_app()
 
