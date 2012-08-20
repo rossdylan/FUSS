d8163eb9b2f9dffbc8c7431a01ed67bf0ae209d7
Ross Delinger <rdelinger@helixoide.com>
2012-08-20 11:07:00 -0400

    Skeleton for the views

--- fuss/views.py
+++ fuss/views.py
@@ -1,12 +1,24 @@
 from pyramid.response import Response
 from pyramid.view import view_config
 
-from sqlalchemy.exc import DBAPIError
 
 from .models import (
     DBSession,
+    FASUser,
+    Topic,
     )
 
 @view_config(route_name='index', renderer='index.mak')
 def index(request):
+    return {}
+
+@view_config(route_name='choose_topics', renderer='prefs.mak')
+def topics(request):
+    return {}
+@view_config(route_name='login', renderer='login.mak')
+def login(request):
+    return {}
+
+@view_config(route_name='logout', renderer='logout.mak')
+def logout(request):
     return {}
