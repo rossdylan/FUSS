76538517ed5d63fbc71042f9f8b5babcc729cffd
Ross Delinger <rossdylan@csh.rit.edu>
2012-08-30 11:34:06 -0400

    fixed the join problems with the model

--- fuss/models.py
+++ fuss/models.py
@@ -20,12 +20,17 @@ DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 Base.query = DBSession.query_property()
 
+association_table = Table('association', Base.metadata,
+        Column('username', Unicode(128), ForeignKey('users.username')),
+        Column('topic', Unicode(128), ForeignKey('topics.name')),)
+
 
 class FASUser(Base):
     __tablename__ = 'users'
     id = Column(Integer, unique=True, primary_key=True)
     username = Column(Unicode(128), unique=True)
-    topics = relationship("Topic", backref='fasuser')
+    topics = relationship("Topic",
+            secondary=association_table)
 
 
 class Topic(Base):
